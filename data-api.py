2aff3c39584d9b965dbbc6139434e85a144205ca
builderpepc <troy.hiruka@gmail.com>
2021-03-28 13:27:53 +0000

    Add municipality list api call

--- data-api.py
+++ data-api.py
@@ -19,5 +19,15 @@ async def all_data(request):
         data = places_puller.main()
     return sanic.response.json(data)
 
+@app.route("/municipalities")
+async def municipalities(request):
+    data = json.load(open('data.json', 'r'))
+    if (datetime.datetime.now() - datetime.datetime.fromtimestamp(data['summary']['timestamp'])).total_seconds() >= 86400:
+        data = places_puller.main()
+    municipalities = []
+    for x in data['results']:
+        if x['municipality'] not in municipalities:
+            municipalities.append(x['municipality'])
+    return sanic.response.json(municipalities)
 
 app.run(debug=True, port='3000', host='0.0.0.0')
